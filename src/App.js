33918c52c37a707b583de4bbd230eaf909b1669c
Shpend Mula <shmula@hotmail.com>
2021-09-25 14:02:57 +0200

    Created Header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import classes from "./App.module.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.app}>
+      <Header />
+      <main>
+        <ul className={classes.list}>
+          <li>Telephone</li>
+        </ul>
+      </main>
     </div>
   );
 }
